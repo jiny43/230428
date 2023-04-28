54687fa007289093836290641ff52a8573db688b
jieun <je_eun2735@naver.com>
2023-04-28 11:42:25 +0900

    html

--- index.js
+++ index.js
@@ -157,4 +157,16 @@ nquirer.prompt(questions).then((answers) => {
     answers
   );
   //{},options,answers 형태로 하나의 객체를 만듦
+  const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    <body>
+      
+    </body>
+    </html>`;
 });
