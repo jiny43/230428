717f1e18eaea0bd8e32224a418f46b61492b9017
jieun <je_eun2735@naver.com>
2023-04-28 10:24:02 +0900

    피자 선택하는 예제코드

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 
 // program.parse(process.argv);
 
-//npm commander.js 사용법
+//npm commander.js 사용법 예시
 // const { program } = require("commander");
 
 // program.option("--first").option("-s, --separator <char>");
@@ -20,26 +20,52 @@
 //node index.js -s /--first a/b/c
 //[ 'a/b/c' ]
 
-const { Command } = require("commander");
-const program = new Command();
+// const { Command } = require("commander");
+// const program = new Command();
 
-program
-  .name("string-util")
-  .description("CLI to some JavaScript string utilities")
-  .version("0.8.0");
+// program
+//   .name("string-util")
+//   .description("CLI to some JavaScript string utilities")
+//   .version("0.8.0");
 
-program
-  .command("split")
-  .description("Split a string into substrings and display as an array")
-  .argument("<string>", "string to split")
-  .option("--first", "display just the first substring")
-  .option("-s, --separator <char>", "separator character", ",")
-  .action((str, options) => {
-    const limit = options.first ? 1 : undefined;
-    console.log(str.split(options.separator, limit));
-  });
+// program
+//   .command("split")
+//   .description("Split a string into substrings and display as an array")
+//   .argument("<string>", "string to split")
+//   .option("--first", "display just the first substring")
+//   .option("-s, --separator <char>", "separator character", ",")
+//   .action((str, options) => {
+//     const limit = options.first ? 1 : undefined;
+//     console.log(str.split(options.separator, limit));
+//   });
 
-program.parse();
+// program.parse();
 //node index.js help split
 //ode index.js split --separator=/ ?/!/@
 // [ '?', '!', '@' ]
+
+// CommonJS (.cjs) npm 예제코드
+const { Command } = require("commander");
+const program = new Command();
+//첫번째 인자로 옵션의 이름을, 두 번째 인자로 옵션의 설명
+//<>로 감싸진 값은 필수값을 의미.
+program
+  .option("-d, --debug", "output extra debugging")
+  .option("-s, --small", "small pizza size")
+  .option("-p, --pizza-type <type>", "flavour of pizza");
+
+//srgv 배열을 파싱하여 명령 줄 인자를 해석하고,
+program.parse(process.argv);
+//program.opts()사용하여 파싱 된 옵션 객체를 반환한다.
+const options = program.opts();
+
+//if문을 사용해서 옵션 객체의 각 프로퍼티를 검사하고, 해당 옵션에 대한 메세지를 출력한다.
+if (options.debug) console.log(options);
+console.log("pizza details:");
+if (options.small) console.log("- small pizza size");
+if (options.pizzaType) console.log(`- ${options.pizzaType}`);
+//node index.js --debug --small --pizza-type=cheese
+//{ debug: true, small: true, pizzaType: 'cheese' }
+//pizza details:
+//- small pizza size
+//- cheese
