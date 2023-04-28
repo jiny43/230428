98471f6a4969e1c73d2f47a5eaad1f907553ff58
jieun <je_eun2735@naver.com>
2023-04-28 09:53:58 +0900

    help

--- index.js
+++ index.js
@@ -8,12 +8,35 @@
 // program.parse(process.argv);
 
 //npm commander.js 사용법
-const { program } = require("commander");
+// const { program } = require("commander");
 
-program.option("--first").option("-s, --separator <char>");
+// program.option("--first").option("-s, --separator <char>");
+
+// program.parse();
+
+// const options = program.opts();
+// const limit = options.first ? 1 : undefined;
+// console.log(program.args[0].split(options.separator, limit));
+//node index.js -s /--first a/b/c
+//[ 'a/b/c' ]
+
+const { Command } = require("commander");
+const program = new Command();
+
+program
+  .name("string-util")
+  .description("CLI to some JavaScript string utilities")
+  .version("0.8.0");
+
+program
+  .command("split")
+  .description("Split a string into substrings and display as an array")
+  .argument("<string>", "string to split")
+  .option("--first", "display just the first substring")
+  .option("-s, --separator <char>", "separator character", ",")
+  .action((str, options) => {
+    const limit = options.first ? 1 : undefined;
+    console.log(str.split(options.separator, limit));
+  });
 
 program.parse();
-
-const options = program.opts();
-const limit = options.first ? 1 : undefined;
-console.log(program.args[0].split(options.separator, limit));
