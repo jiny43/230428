2b4847f036c56b2cb72c38446ebd626736b958c4
jieun <je_eun2735@naver.com>
2023-04-28 09:50:03 +0900

    npm 사용법

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+// const { program } = require("commander");
+
+// const app = program.command("kdt");
+// app.action(() => {
+//   console.log("wow!내가 만든 명렁어!");
+// });
+
+// program.parse(process.argv);
+
+//npm commander.js 사용법
 const { program } = require("commander");
 
-const app = program.command("kdt");
-app.action(() => {
-  console.log("wow!내가 만든 명렁어!");
-});
+program.option("--first").option("-s, --separator <char>");
 
-program.parse(process.argv);
+program.parse();
+
+const options = program.opts();
+const limit = options.first ? 1 : undefined;
+console.log(program.args[0].split(options.separator, limit));
